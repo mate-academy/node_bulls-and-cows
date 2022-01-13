d0c493b128d1388be37afa3f8c265f625c6767dd
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-01-13 23:27:04 +0200

    Bulls and cows

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const readline = require('readline');
+const { bull } = require('./bull');
+const { cows } = require('./cows');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Can you guess numbers?: ', (inpTer) => {
+  const resBull = bull(inpTer);
+  const resCows = cows(inpTer);
+
+  // eslint-disable-next-line no-console
+  console.log(`result ${resBull} bull and ${resCows} cows`);
+  terminal.close();
+});
