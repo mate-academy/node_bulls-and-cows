bd428f5a5aaeeb2cb397b5697288dcf953238948
IvanKolodiazhnyi <kysok.usera1337@gmail.com>
2022-01-28 02:00:20 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+const readline = require('readline');
+const { game } = require('./bulls_or_cows');
+const { randomNumber } = require('./random_number');
+
+function gameBullsAndCows() {
+  const anyNymber = randomNumber();
+
+  const recursiveGame = function() {
+    const terminal = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    terminal.question(
+      'The "Bulls & Cows" game! Please write your four digits number ',
+      async(userNumber) => {
+        const userNumberCut = userNumber.slice(0, 4);
+        const answer = game(anyNymber, userNumberCut);
+
+        if (answer.includes('Greate')) {
+          terminal.close();
+        } else {
+          terminal.close();
+          recursiveGame();
+        }
+      }
+    );
+  };
+
+  return recursiveGame();
+};
+
+gameBullsAndCows();
