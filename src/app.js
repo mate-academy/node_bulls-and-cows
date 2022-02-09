316c4383ba9760188003ebb49467da6495a0f649
Taras Holiuk <taras.holiuk@vestr.com>
2022-02-09 13:47:16 +0200

    implement Bulls and Cows game

--- src/app.js
+++ src/app.js
@@ -8,93 +8,100 @@ const terminal = readline.createInterface({
 });
 
 const question = (args) => new Promise((res) => terminal.question(args, res));
+const DIGITS_COUNT = 4;
 
-const generatedNumber = 1110//(Math.random().toFixed(4) * 10000);
+const generatedNumber = (Math.random().toFixed(DIGITS_COUNT) * 10000);
 const BULL = 'BULL';
 const COW = 'COW';
 
 function paramsValidation(args) {
-  return args.length === 4 && Number.isInteger(+args[0]);
+  return args.length === DIGITS_COUNT && Number.isInteger(+args[0]);
 };
 
-function checkRole(baseNumberArray, digitToFind, digitIndex) {
-  let bull = false;
-  let cow = false;
+function roleCheck({
+  generatedNumberArray, 
+  digitToFind, 
+  digitIndex
+}) {
+  let bullIndex = null;
+  let cowIndex = null;
 
-  baseNumberArray.forEach((number, index) => {
+  generatedNumberArray.forEach((number, index) => {
     if (number === digitToFind &&
       index === digitIndex) {
-        bull = true;
+        bullIndex = index;
+        return;
       }
 
     if (number === digitToFind &&
       index !== digitIndex) {
-        cow = true;
+        cowIndex = index;
+        return;
       } 
   });
 
-  if (bull) {
-    return BULL;
-  } else if (cow) {
-    return COW;
+  if (bullIndex) {
+    return { role: BULL, reservedIndex: bullIndex };
+  } else if (cowIndex) {
+    return { role: COW, reservedIndex: cowIndex };
   } else {
-    return '';
+    return { role: null, reservedIndex: null };
   }
 }
 
 function numberCheck(num) {
-  let generatedNumberArray = generatedNumber.toString().split('');
+  const generatedNumberArray = generatedNumber.toString().split('');
   const numArray = num.toString().split('');
-  const roles = [];
-  const reservedRoles = [null, null, null, null];
-  const totalNumbers = { bulls: 0, cows: 0 };
+  const foundRoles = new Array(DIGITS_COUNT).fill(null);
+  const totalCount = { bulls: 0, cows: 0 };
 
   numArray.forEach((digit, place) => {
-    const role = checkRole(generatedNumberArray, digit, place);
-    roles.push(role);
+    const { role, reservedIndex } = roleCheck({
+      generatedNumberArray,
+      digitToFind: digit,
+      digitIndex: place,
+    });
+
+    if (role && foundRoles[reservedIndex] !== BULL) {
+      foundRoles.splice(reservedIndex, 1, role);
+    }
   });
 
-  console.log(roles);
-
-  roles.forEach(role => {
+  foundRoles.forEach(role => {
     switch(role) {
       case BULL:
-        totalNumbers.bulls += 1;
+        totalCount.bulls += 1;
         break;
       case COW:
-        totalNumbers.cows += 1;
+        totalCount.cows += 1;
         break;
       default:
         break;
     }
   });
 
-  return totalNumbers;
+  return totalCount;
 };
 
 async function game(text) {
   const params = await question(text);
 
-  try {
     if (!paramsValidation(params)) {
       terminal.write('Invalid entered parameters \n');
       game('Try again \n');
     } else {
       const { bulls, cows } = numberCheck(params);
   
-      if (bulls === 4) {
+      if (bulls === DIGITS_COUNT) {
         terminal.write('You win! \n');
         terminal.close();
       } else {
         terminal.write(`${cows === 1 ? 'cow' : 'cows'}: ${cows} ` +
-          `${bulls === 1 ? 'bull' : 'bulls'}: ${bulls}\n`);
+          `${bulls === 1 ? 'bull' : 'bulls'}: ${bulls} \n`);
 
-        game('Try again \n');
+        game('Try again ;) \n');
       }
     };
-  } catch {
-    terminal.write('Try again');
-  }
 };
 
 game('Guess the number \n');
