3570b73433810012ebb2d2ae2c1dfd3ed40fa85d
Nikita Karbazin <karbazinnikita@gmail.com>
2022-07-27 16:11:37 +0300

    fixed logic

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const readline = require('readline');
 const _ = require('lodash');
 
 const number = _.random(1000, 9999);
-const bullAndCows = [];
+let bullAndCows = [];
 
 const terminal = readline.createInterface({
   input: process.stdin,
@@ -19,27 +19,32 @@ function f() {
       console.log('You win!');
       terminal.close();
     } else {
-      const numArr = number.toString().split('').map((n, i) => [n, i]);
-      const guessedNumArr = guessedNumber.split('').map((n, i) => [n, i]);
+      const numArr = number.toString().split('');
+      const guessedNumArr = guessedNumber.split('');
 
-      for (let i = 0; i < numArr; i++) {
-        if (numArr[i][1] === guessedNumArr[i][1]) {
+      for (let i = 0; i < guessedNumArr.length; i++) {
+        if (+numArr[i] === +guessedNumArr[i]) {
           bullAndCows.push('bull');
+          continue;
         }
 
-        for (let j = 0; j < numArr; j++) {
-          if (+numArr[i][0] === +guessedNumArr[j][0]) {
-            bullAndCows.push('cow');
-          }
+        if (numArr.includes(guessedNumArr[i])) {
+          bullAndCows.push('cow');
         }
       }
 
       let bullsCount = 0;
       let cowsCount = 0;
 
-      bullAndCows.forEach(str => str === 'bull' ? bullsCount++ : cowsCount++);
+      bullAndCows.forEach(str => {
+        str === 'bull' ? bullsCount += 1 : cowsCount += 1;
+      });
       // eslint-disable-next-line no-console
       console.log(`${bullsCount}: bulls ${cowsCount}: cows! Try again`, number);
+
+      bullsCount = 0;
+      cowsCount = 0;
+      bullAndCows = [];
 
       return f();
     }
