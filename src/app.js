da5c11fd6b4b01c8e80d9facb41a8d85bb312885
Bohdan Poshtarenko <mira1zu733@gmail.com>
2022-09-09 20:35:12 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const { getRandomInteger } = require('./getRandomInteger');
+
+const game = require('./game');
+
+const target = String(getRandomInteger(1000, 9999));
+
+game(target);
