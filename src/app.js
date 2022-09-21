a29a6a155d5407ac723f92de2f1ac1f923ea5900
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-21 17:09:17 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { terminal } = require('./terminal');
+const { calculation } = require('./calculation');
+
+terminal.question('Gues a number with 4 digit\n', (gues) => calculation(gues));
