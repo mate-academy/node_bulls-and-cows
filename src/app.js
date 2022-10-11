9a9c497c448cf2d790e94953f0d3c26b8ed88751
Anton Kulchytskyi <antek1809@gmail.com>
2022-10-11 02:43:40 +0200

    solution node bulls and cows

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const { randomNumbers } = require('./randomNumbers');
+const { terminal } = require('./terminal');
+const { cntBullsAndCows } = require('./cntBullsAndCows');
+
+let attemptCnt = 1;
+
+const userAnswer = () => {
+  terminal.question('Please enter 4 different digits\n', (numFromUser) => {
+    if (numFromUser === randomNumbers) {
+      terminal.write(`You guessed the number ${randomNumbers} \n
+        after ${attemptCnt} attempt(s)`);
+      terminal.close();
+    } else if (numFromUser.length === 4) {
+      terminal.write(`attempt #${attemptCnt} \n`
+       + cntBullsAndCows(randomNumbers, numFromUser) + '\n');
+      attemptCnt += 1;
+      userAnswer();
+    } else {
+      attemptCnt += 1;
+      userAnswer();
+    }
+  });
+};
+
+userAnswer();
---
+++ src/cntBullsAndCows.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function cntBullsAndCows(randomNum, userNum) {
+  let bullsCnt = 0;
+  let cowsCnt = 0;
+
+  for (let i = 0; i < randomNum.length; i += 1) {
+    if (randomNum[i] === userNum[i]) {
+      bullsCnt += 1;
+    } else if (randomNum.includes(userNum[i])) {
+      cowsCnt += 1;
+    }
+  }
+
+  return `Bulls => ${bullsCnt} and Cows => ${cowsCnt}`;
+
+  // return [bullsCnt, cowsCnt];
+};
+
+module.exports.cntBullsAndCows = cntBullsAndCows;
