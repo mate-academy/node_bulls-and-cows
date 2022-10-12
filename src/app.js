f41f5cfca9a1f69e3f334c13b5bf38afaa0e583a
Denys Korotkoruchko <denenergii@gmail.com>
2022-10-12 21:27:58 +0300

    ready

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
 'use strict';
+const redline = require('readline');
+const { randomizer } = require('./helpers')
+
+const terminal = redline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const random = randomizer();
+
+const game = () => {
+  terminal.question('enter 4 numbers ', (input) => {
+    if (input.trim().length > 4) {
+      console.log('more then 4 numbers');
+    } else if (input.trim().length < 4) {
+      console.log('less then 4 numbers');
+    } else {
+      for (let i = 0; i < 4; i++) {
+        if (random[i] === input[i]) {
+          console.log('bull');
+        } else if (random.includes(input[i])) {
+          console.log('cow');
+        }
+      }
+    }
+
+    if (random !== input) {
+      game();
+    } else if(random === input) {
+      console.log('You win!')
+      terminal.close;
+    }
+  })
+};
