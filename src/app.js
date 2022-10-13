3081539ddeff4431e4af9d66dd3e3397e3238a4c
Jamal Amoian <leonidagutin09@gmail.com>
2022-10-13 13:01:21 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const readline = require('readline');
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const randomNumbers = Math.round(Math.random() * 10000).toString();
+let str = '';
+
+userInterface.question('Write 4 digits ', (number) => {
+  const formatNumber = number.toString();
+
+  if (formatNumber.length !== 4) {
+    throw Error('You writed not 4 digits');
+  }
+
+  for (let i = 0; i < formatNumber.length; i++) {
+    if (number[i] === randomNumbers[i]) {
+      str += 'bull ';
+    } else {
+      str += 'cows ';
+    }
+  }
+
+  console.log(randomNumbers);
+
+  console.log(str);
+
+  userInterface.close();
+});
+
+// function generateRandomNumber() {
+//   return Math.round(Math.random() * 10000);
+// }
+
+// function calculatingBullsAndCows() {
+
+// }
