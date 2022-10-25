0dbcde43fc21a357fd84073579f186b4e819404a
PavloMarkov <r9markovpn@gmail.com>
2022-10-25 16:14:47 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const { generateDigits, inputResultCheck } = require('./generate');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const randomDigits = generateDigits();
+let answer = '';
+
+const ask = () => {
+  terminal.question(
+    'enter number with 4 different digits - try to guess PC number ',
+    (num) => {
+      answer = num;
+      inputResultCheck(num, randomDigits);
+      console.log(`PC is ${randomDigits}, your number is ${num}`);
+
+      if (answer === randomDigits) {
+        console.log(`You win the game!`);
+        terminal.close();
+      }
+
+      if (answer !== randomDigits) {
+        ask();
+      }
+    });
+};
+
+ask();
