8473c3d7ca37395d868b0b74716a95c8904a199b
PavloMarkov <r9markovpn@gmail.com>
2022-10-25 16:34:15 +0300

    input checking added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 
 const readline = require('readline');
 const { generateDigits, inputResultCheck } = require('./generate');
+const _ = require('lodash');
 
 const terminal = readline.createInterface({
   input: process.stdin,
@@ -16,6 +17,19 @@ const ask = () => {
   terminal.question(
     'enter number with 4 different digits - try to guess PC number ',
     (num) => {
+      if (num.length !== 4) {
+        console.log('Enter 4 digits');
+
+        return ask();
+      } else if (_.uniq(num.split('')).length < 4) {
+        console.log('Numbers should be uniq!');
+
+        return ask();
+      } else if (Number.isNaN(+num) || num.includes('e')) {
+        console.log('Only numbers are expected!');
+
+        return ask();
+      }
       answer = num;
       inputResultCheck(num, randomDigits);
       console.log(`PC is ${randomDigits}, your number is ${num}`);
