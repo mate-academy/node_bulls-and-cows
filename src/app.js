c3ca5df85db5330dcf1a4b2fc246167ca97c0d1d
Mariia Datsenko <mariy.datsenko@gmail.com>
2022-11-16 13:04:33 +0200

    game has been created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const inquirer = require('inquirer');
+const { randomizer } = require('./randomizer');
+const { game } = require('./game');
+
+const terminal = async() => {
+  const { userInput } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'userInput',
+      message: 'Try to guess secret number: ',
+    },
+  ]);
+
+  const userNumber = userInput.split('');
+
+  const randomNumber = randomizer();
+
+  const games = game(randomNumber, userNumber);
+
+  if (games === true) {
+    console.log('Wow, awesome!');
+  } else {
+    console.log(games);
+    terminal();
+  }
+};
+
+terminal();
