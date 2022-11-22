fbe6f3901c7316b8c3490183fb979ea10085102b
Serhii Romaniuk <deepspark@gmail.com>
2022-11-22 09:32:36 +0200

    minor fix

--- src/calculateBullsCows.js
+++ src/calculateBullsCows.js
@@ -3,21 +3,14 @@
 const calculator = (secret, guess) => {
   let cowsCount = 0;
   let bullsCount = 0;
-  const secretCopy = [];
-  const guessCopy = [];
 
   for (let i = 0; i < secret.length; i++) {
     if (secret[i] === guess[i]) {
       bullsCount++;
     } else {
-      secretCopy.push(secret[i]);
-      guessCopy.push(guess[i]);
-    }
-  }
-
-  for (const num of secretCopy) {
-    if (guessCopy.some(elem => elem === num)) {
-      cowsCount++;
+      if (guess.includes(secret[i])) {
+        cowsCount++;
+      }
     }
   }
 
