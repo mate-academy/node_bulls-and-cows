bdc5bc55be7576c3830eb51ac962a48952555f13
M.Ali Yücesoylu <myucesoylu@gmail.com>
2023-01-24 15:53:15 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+const { generateRandomNumber } = require("./randomNumberGenerator");
+const { terminalOperator } = require("./terminalOperator");
+
+const randomNumber = generateRandomNumber();
+
+terminalOperator("Please guess a number from 1000 to 9999 \n", randomNumber);
