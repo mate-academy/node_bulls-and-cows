8b4765a0eecdbc4bf3739fb3b82e96bc9a20e347
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-02-12 18:27:31 +0200

    bullsAndCows corrected

--- src/bullsAndCows.js
+++ src/bullsAndCows.js
@@ -4,14 +4,10 @@ const bullsAndCows = (generated, entered) => {
   let bulls = 0;
   let cows = 0;
 
-  for (let i = 0; i < generated.length; i++) {
+  for (let i = 0; i < 4; i++) {
     if (generated[i] === entered[i]) {
       bulls++;
-
-      continue;
-    }
-
-    if (generated.includes(entered[i])) {
+    } else if (generated.includes(entered[i])) {
       cows++;
     }
   }
