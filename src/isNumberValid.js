951251c7fc4be396ea7718fe03c05dcd0e607a3b
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-02-12 16:19:44 +0200

    checking if number is valid

---
+++ src/isNumberValid.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const isNumberValid = (number) => {
+  if (number.length !== 4 || !isNaN(+number)) {
+    return false;
+  }
+
+  const uniqueElements = new Set(number);
+
+  if (uniqueElements.length !== number.length) {
+    return false;
+  }
+
+  return true;
+};
+
+module.exports = { isNumberValid };
