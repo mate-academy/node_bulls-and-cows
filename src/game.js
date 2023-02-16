45d8752d831b682974637bbbeac42da48f8f4f8e
Kris Planka <kristyaplanka@gmail.com>
2023-02-16 18:10:09 +0200

    const name changed

--- src/game.js
+++ src/game.js
@@ -11,15 +11,15 @@ const terminal = readline.createInterface({
 
 const game = () => {
   terminal.question('Guess a number: ', (number) => {
-    const fourUniqueDigits = 4;
+    const uniqueDigitsAmount = 4;
     const numberTypeNumber = Number(number);
-    const invalidNumber = number.length !== fourUniqueDigits
-    || isNaN(numberTypeNumber) || new Set(number).size !== fourUniqueDigits;
+    const invalidNumber = number.length !== uniqueDigitsAmount
+    || isNaN(numberTypeNumber) || new Set(number).size !== uniqueDigitsAmount;
 
     if (invalidNumber) {
       // eslint-disable-next-line no-console
       console.log(`Number should includes
-      ${fourUniqueDigits} different digits, try again!`);
+      ${uniqueDigitsAmount} different digits, try again!`);
 
       return game();
     }
