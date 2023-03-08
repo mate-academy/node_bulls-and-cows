f96b218abe91fba547da13647b62a41065a1873b
Ivan Stadnytskyy <vanst1@icloud.com>
2023-03-08 22:11:47 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const { numberGenerator } = require('./numberGenerator');
+const { cowBullCalculator } = require('./cowBullCalculator');
+const { inputValidator } = require('./inputValidator');
+const readline = require('readline');
+
+const number = numberGenerator();
+const startMessage = 'Guess the number: ';
+const errorMessage = 'Error: invalid number format, try again: ';
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question(startMessage, async(userNumber) => {
+  let inputNumber = userNumber;
+
+  cowBullCalculator(number, inputNumber);
+
+  while (number !== inputNumber) {
+    await new Promise((resolve) => {
+      terminal.question(inputValidator(inputNumber)
+        ? startMessage
+        : errorMessage,
+      (uNumber) => {
+        inputNumber = uNumber;
+        cowBullCalculator(number, uNumber);
+        resolve();
+      });
+    });
+  }
+
+  terminal.close();
+});
