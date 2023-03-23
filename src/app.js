16bb41d76b1748aa6278e8911f54cce58554d8ab
OlhaArama <arama_o_f@ukr.net>
2023-03-23 20:55:15 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const { terminal } = require('./terminal.js');
+const { compareNumbers } = require('./compareNumbers.js');
+
+terminal.question('Guess the number. ', compareNumbers);
