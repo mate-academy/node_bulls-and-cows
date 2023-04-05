9ec9051433918ea9168ae3750a2e963de337d86f
Rostyslav Horytskyi <gorzer.rostyslav@gmail.com>
2023-04-05 12:30:48 +0300

    create game

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const { letsPlay } = require('./letsPlay');
+
+letsPlay();
---
+++ src/terminal.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const readline = require('readline');
+
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+module.exports = { readlineInterface };
