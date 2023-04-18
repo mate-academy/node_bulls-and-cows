ff097197e8030ccbae57f12816a1a2e34d988847
Dima Cherednyk <dicherednyk@gmail.com>
2023-04-18 21:06:21 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { checkNumber } = require('./checkNumber');
+const { terminal, closeTerminal } = require('./termimal');
+
+terminal.question('Do you want to play?', (answer) => {
+  if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+    return terminal.question('Enter a 4 different digit number.', (number) => {
+      return checkNumber(number);
+    });
+  }
+
+  console.log('ok(');
+
+  return closeTerminal();
+});
