a05f169fc510bbc3385abd34d837ff0193cf5949
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-04-26 22:54:20 +0300

    created game

--- src/app.js
+++ src/app.js
@@ -1,39 +1,41 @@
 /* eslint-disable no-console */
 'use strict';
 
-const { terminal } = require('./terminal');
-const { generateNumber } = require('./generator');
 const { compareNumbers } = require('./compareNumber');
+const { generateNumber } = require('./generator');
+const { terminal } = require('./terminal');
 
 const generatedNumber = generateNumber();
 
-const playBullsAndCows = () => {
-  terminal.question('Guess the number ', (userNumber) => {
-    const isUserNumberValid = new Set(userNumber).size === 4
-    && !isNaN(userNumber);
+const checkUserNumber = (userNumber) => {
+  const isUserNumberValid = new Set(userNumber).size === 4
+  && !isNaN(userNumber);
 
-    if (!isUserNumberValid) {
-      console.log('Insert a number of 4 different digits');
-
-      playBullsAndCows();
-
-      return;
-    }
-
-    const { bull, cow } = compareNumbers(userNumber, generatedNumber);
-
-    if (bull === 4) {
-      console.log('U won, congratulations!');
-
-      terminal.close();
-    }
-
-    console.log(
-      `Try again, u have ${bull} bulls and ${cow} cows`
-    );
+  if (!isUserNumberValid) {
+    console.log('Insert a number of 4 different digits');
 
     playBullsAndCows();
-  });
+
+    return;
+  }
+
+  const { bull, cow } = compareNumbers(userNumber, generatedNumber);
+
+  if (bull === 4) {
+    console.log('U won, congratulations!');
+
+    terminal.close();
+  }
+
+  console.log(
+    `Try again, u have ${bull} bulls and ${cow} cows`
+  );
+
+  playBullsAndCows();
+};
+
+const playBullsAndCows = () => {
+  terminal.question('Guess the number ', checkUserNumber);
 };
 
 playBullsAndCows();
