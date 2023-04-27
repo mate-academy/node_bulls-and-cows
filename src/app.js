198fd972dddd9e6fcd53c9fc8202c4ebfe6e1e99
milenadiamonds1 <milenadubova16@gmail.com>
2023-04-28 01:15:27 +0300

    created game

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { generateRandomNumber } = require('./generateRandomNumber');
+const { isValid } = require('./isInputValid');
+const { terminal } = require('./terminal');
+const { calculateBullsAndCows } = require('./calculateCowsAndBulls');
+
+const generetadNum = generateRandomNumber();
+
+const checkUserAnswer = (answer) => {
+  if (!isValid) {
+    console.log('Insert a number of 4 different digits');
+
+    terminal.question(
+      'Please write a number of 4 different digits',
+      checkUserAnswer
+    );
+
+    return;
+  }
+
+  const { bulls, cows } = calculateBullsAndCows(generetadNum, answer);
+
+  if (bulls === 4) {
+    console.log(`Congratulations, you won!`);
+    terminal.close();
+
+    return;
+  }
+  console.log(`${bulls} bulls, ${cows} cows`);
+  playGame();
+};
+
+const playGame = () => {
+  terminal.question(
+    'Please write a number of 4 different digits',
+    checkUserAnswer
+  );
+};
+
+playGame();
