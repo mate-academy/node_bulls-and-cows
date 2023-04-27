0276b916ae62c23d42bf59a474fbe278cf907868
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-04-27 19:52:28 +0300

    messages were added

---
+++ src/messages.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const welcomeMessage = '\nWelcome to bulls and cows game!';
+const enterMessage = '\nEnter a number of 4 different digits: ';
+const errorMessage = '\nError. Wrong input.';
+const congratsMessage = '\nCongrats!!! You won!!!';
+
+module.exports = {
+  welcomeMessage,
+  errorMessage,
+  enterMessage,
+  congratsMessage,
+};
