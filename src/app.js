e8415799faa96036d522e93b96e921af9105f35b
Bogdan <bodia-00wot@ukr.net>
2023-04-28 18:27:34 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const { generateArrayOfNumbers } = require('./generateArrayOfRandomNumbers.js');
+const { createTerminal } = require('./createTerminal.js');
+const { checkNumbers } = require('./checkNumbers.js');
+
+const launchApp = () => {
+  const generatedArrOfNumbers = generateArrayOfNumbers();
+
+  const terminal = createTerminal();
+
+  checkNumbers(terminal, generatedArrOfNumbers);
+};
+
+launchApp();
