95f1b10afa7cb9ca6b66f413eb12e6e12bc45e50
Vladimir Yurkov <vladimir.yurkov.91@gmail.com>
2023-05-20 18:36:33 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
 'use strict';
+/* eslint-disable no-console */
+
+const readline = require('readline');
+const { calculatingBullsAndCows } = require('./calculatingBullsAndCows');
+const { generateNumber } = require('./generateNumber');
+
+const randomNumber = generateNumber();
+const question = 'Enter a number with 4 digits to guess: ';
+
+console.log(randomNumber);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const isValidAnswer = (answer) => {
+  return new Set(answer).size === 4 && !isNaN(answer);
+};
+
+const handleUserInput = (answer) => {
+  if (!isValidAnswer(answer)) {
+    console.log('Please enter valid input');
+    launchGame();
+
+    return;
+  }
+
+  const {
+    status,
+    statusString,
+  } = calculatingBullsAndCows(randomNumber, answer);
+
+  if (status) {
+    console.log(`Congratulations, you win!`);
+    rl.close();
+
+    return;
+  }
+
+  console.log(statusString);
+  launchGame();
+};
+
+const launchGame = () => {
+  rl.question(question, handleUserInput);
+};
+
+launchGame();
