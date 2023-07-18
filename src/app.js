e21189577c957678a2b280b4188a6d5afbe3c21b
Paweł Smoliński <ski1993@o2.pl>
2023-07-18 14:48:04 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { createInterface } = require('readline');
+const { generateNumbers } = require('./generateNumbers');
+const { check } = require('./checkInput');
+
+const game = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const number = generateNumbers();
+
+const runGame = () => {
+  game.question('Guess the number! ', (guess) => {
+    const stringToCheck = new Set(guess
+      .split('')
+      .map(Number));
+
+    if (stringToCheck.size === 4) {
+      const answer = check(number, guess);
+      const bulls = answer[0];
+      const cows = answer[1];
+
+      if (bulls === 4) {
+        console.log('You won');
+        game.close();
+      } else {
+        console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+        runGame();
+      }
+    } else {
+      console.log('Provide 4 different digits');
+      runGame();
+    }
+  });
+};
+
+runGame();
