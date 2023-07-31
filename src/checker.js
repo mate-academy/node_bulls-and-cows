3c6d83b8e7be7dec2d71b7b839dc1a8ff6401c80
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-07-31 17:01:27 +0300

    Adde new checks

--- src/checker.js
+++ src/checker.js
@@ -6,7 +6,9 @@ function checker(userNumber, randomNumber) {
   for (let i = 0; i < userNumber.length; i++) {
     if (randomNumber.includes(userNumber[i])) {
       result[i] = 'cow';
-    };
+    } else {
+      result[i] = '*';
+    }
 
     if (userNumber[i] === randomNumber[i]) {
       result[i] = 'bull';
