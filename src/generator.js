3e79a4b096170edc9fbe56c405c7de652e93e184
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-08-06 22:06:46 +0300

    fix generator

--- src/generator.js
+++ src/generator.js
@@ -1,7 +1,19 @@
 'use strict';
 
 const generateRandomNumber = () => {
-  return Math.ceil(Math.random() * 10000);
+  const uniqueDigits = [];
+
+  while (uniqueDigits.length < 4) {
+    const digit = Math.floor(Math.random() * 10);
+
+    if (!uniqueDigits.includes(digit)) {
+      uniqueDigits.push(digit);
+    }
+  }
+
+  const resultNumber = uniqueDigits.join('');
+
+  return parseInt(resultNumber);
 };
 
 module.exports = { generateRandomNumber };
