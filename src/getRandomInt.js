b6e84138a3c97d0ab0da904aa35c763839836b27
IlonaBorkovska <borkovska.io@gmail.com>
2023-08-16 16:12:49 +0300

    fix mistake

--- src/getRandomInt.js
+++ src/getRandomInt.js
@@ -4,7 +4,7 @@ function getRandomInt() {
   const nums = [];
 
   while (nums.length !== 4) {
-    const randomNum = Math.floor(Math.random() * (9 - 1 + 1) + 1);
+    const randomNum = Math.floor(Math.random() * 9 + 1);
 
     if (!nums.includes(randomNum)) {
       nums.push(randomNum);
