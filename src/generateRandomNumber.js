7f01d2c9e21afe105c81630584ab5ac7f1ad4428
Vasyl Kozak <sasdaiv@gmail.com>
2023-08-17 19:20:57 +0300

    fixes 3

--- src/generateRandomNumber.js
+++ src/generateRandomNumber.js
@@ -3,10 +3,11 @@
 const generateRandomNumber = () => {
   const randomDigits = [];
 
-  randomDigits.push(Math.floor(Math.random() * (5 - 3 + 1)) + 3);
-  randomDigits.push(Math.floor(Math.random() * (7 - 6 + 1)) + 6);
-  randomDigits.push(Math.floor(Math.random() * (9 - 8 + 1)) + 8);
-  randomDigits.push(Math.floor(Math.random() * (2 - 0 + 1)) + 0);
+  for (let i = 0; i < 4; i++) {
+    const randomNumber = Math.floor(Math.random() * 9000) + 1000;
+
+    randomDigits.push(randomNumber);
+  }
 
   randomDigits.sort(() => Math.random() - 0.5);
 
