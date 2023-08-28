5a3fc4ccee213ff5a10e71bf8b3c7ae362ae674a
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-08-28 12:30:15 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const readline = require('readline');
+const { generateRandomNumber } = require('./numberGenerator');
+const { calculateBullsAndCows } = require('./bullsAndCowsCalculator');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const secretNumber = generateRandomNumber();
+
+function playGame() {
+  rl.question('Enter your guess: ', (guessedNumber) => {
+    if (guessedNumber.length !== 4 || !/^\d{4}$/.test(guessedNumber)) {
+      playGame();
+
+      return;
+    }
+
+    const result = calculateBullsAndCows(secretNumber, guessedNumber);
+
+    if (result.bulls === 4) {
+      rl.close();
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+      playGame();
+    }
+  });
+}
+
+playGame();
