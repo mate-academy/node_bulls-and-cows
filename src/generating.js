ea87858ffc981f05d560ea8886845522f9c1ce0e
Vlad Kurachenko <x0674016244@gmail.com>
2023-08-31 14:26:49 +0300

    upd generating

--- src/generating.js
+++ src/generating.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const generating = () => {
-  const randomDigits = [];
+  const randomDigits = [
+    Math.floor(Math.random() * 3) + 3,
+    Math.floor(Math.random() * 2) + 6,
+    Math.floor(Math.random() * 2) + 8,
+    Math.floor(Math.random() * 3),
+  ];
 
-  randomDigits.push(Math.floor(Math.random() * (5 - 3 + 1)) + 3);
-  randomDigits.push(Math.floor(Math.random() * (7 - 6 + 1)) + 6);
-  randomDigits.push(Math.floor(Math.random() * (9 - 8 + 1)) + 8);
-  randomDigits.push(Math.floor(Math.random() * (2 - 0 + 1)) + 0);
-
-  (function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  })(randomDigits);
+  randomDigits.sort(() => Math.random() - 0.5);
 
   return randomDigits.join('');
 };
