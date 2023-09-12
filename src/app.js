70926c6c252739fb9e7b55d96efca2251b570ddd
Illia Vinkov <illia.vinkov@gmail.com>
2023-09-12 07:16:50 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -11,9 +11,15 @@ const rl = readline.createInterface({
 
 const secretNumber = generateRandomNumber();
 
+function log(message) {
+  process.stdout.write(`${message}\n`);
+}
+
 function playGame() {
   rl.question('Enter your guess: ', (guessedNumber) => {
+    // Check if the guessed number is valid
     if (guessedNumber.length !== 4 || !/^\d{4}$/.test(guessedNumber)) {
+      log('Invalid guess. Please enter a 4-digit number.');
       playGame();
 
       return;
@@ -22,10 +28,9 @@ function playGame() {
     const result = calculateBullsAndCows(secretNumber, guessedNumber);
 
     if (result.bulls === 4) {
+      log('Congratulations! You guessed the correct number!');
       rl.close();
     } else {
-      // eslint-disable-next-line no-console
-      console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
       playGame();
     }
   });
