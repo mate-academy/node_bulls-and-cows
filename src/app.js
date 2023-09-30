736e08481b8175d3896554b5fbad0b8802d2d07e
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-09-30 16:00:26 +0300

    Created game

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const { getRandomNumber } = require('./randomDigits');
+const { calculateResult } = require('./calculateResult');
+
+const gemeratNumber = getRandomNumber();
+
+const terminal = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
+
+console.log('Hi! Try to guess the number.');
+
+const game = () => {
+  terminal.question('Enter your presume: ', (presume) => {
+    const normalizePresume = presume.trim();
+
+    if (normalizePresume.length !== 4 || !/^\d{4}$/.test(normalizePresume)) {
+      console.log('The number must consist of 4 digits');
+      game();
+
+      return;
+    }
+
+    const result = calculateResult(gemeratNumber, normalizePresume.split(''));
+
+    if (result.bulls !== 4) {
+      console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+      game();
+    } else {
+      console.log(`Bulls: ${result.bulls}! You win!`);
+      terminal.close();
+    }
+  });
+};
+
+game();
---
+++ src/calculateResult.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const calculateResult = (answer, assumption) => {
+  let bulls = 0;
+  let cows = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (answer[i] === +assumption[i]) {
+      bulls++;
+    } else if (answer.includes(+assumption[i])) {
+      cows++;
+    }
+  }
+
+  return {
+    bulls,
+    cows,
+  };
+};
+
+module.exports = {
+  calculateResult,
+};
