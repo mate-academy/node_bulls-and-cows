f1b4b5694693b5c3635755afa5fe1260bb710c80
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-09 00:49:45 +0300

    solution

---
+++ src/getRandomNums.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const MAX_LENGTH = require('./constants');
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 9) + 1;
+}
+
+function getRandomNums() {
+  const result = [];
+
+  while (result.length !== MAX_LENGTH) {
+    const newNum = getRandomNum();
+
+    if (!result.includes(newNum)) {
+      result.push(newNum);
+    }
+  }
+
+  return result.join('');
+}
+
+module.exports = {
+  getRandomNums,
+};
