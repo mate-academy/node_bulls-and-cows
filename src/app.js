32ba54d200cd522b2465945f8ca1bd9323327843
neovendano <kolesnyk.vik@gmail.com>
2023-10-09 21:37:55 +0300

    constant name change

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
 'use strict';
+/* eslint-disable no-console */
+
+const {
+  WRONG_INPUT_MESSAGE,
+  WELCOME_MESSAGE,
+  VICTORY_MESSAGE,
+  gameNumberSize,
+} = require('./constants');
+const { calculateBullsCows } = require('./calculateBullsCows');
+const { ioInterface } = require('./ioInterface');
+const { generateFixedSizeNumber } = require('./generateFixedSizeNumber');
+
+const generatedNumber = generateFixedSizeNumber(gameNumberSize);
+
+async function createGameSession(numberSize, gameNumber) {
+  while (true) {
+    const answer = await new Promise((resolve) => {
+      ioInterface.question(WELCOME_MESSAGE, resolve);
+    });
+
+    if (answer.length !== numberSize || !Number(answer)) {
+      console.log(WRONG_INPUT_MESSAGE);
+
+      continue;
+    }
+
+    const userNumber = Number(answer);
+
+    if (gameNumber === userNumber) {
+      console.log(VICTORY_MESSAGE);
+      break;
+    } else {
+      const { bulls, cows } = calculateBullsCows(gameNumber, userNumber);
+      const bullsAndCowsMessage
+        = `Oopsie, there are ${bulls} bulls`
+        + ` and ${cows} cows (^◕ᴥ◕^)`;
+
+      console.log(bullsAndCowsMessage);
+    }
+  }
+
+  ioInterface.close();
+}
+
+createGameSession(gameNumberSize, generatedNumber);
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const gameNumberSize = 4;
+
+const WRONG_INPUT_MESSAGE
+  = 'You printed something wrong (>_<). Pwease, try again (ﾉ◕ヮ◕)ﾉ';
+const WELCOME_MESSAGE
+  = `Enter ${gameNumberSize} digits pwease (◕‿◕✿): `;
+const VICTORY_MESSAGE
+  = 'Wow, you won! (づ ◕‿◕ )づ';
+
+module.exports = {
+  WRONG_INPUT_MESSAGE,
+  WELCOME_MESSAGE,
+  VICTORY_MESSAGE,
+  gameNumberSize,
+};
