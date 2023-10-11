2a635f631e4189ae14c2b01e6582cca2cc045ea8
KatOlista^^! <olya.katyushina@gmail.com>
2023-10-11 18:32:11 +0200

    fix redundant nested if/else

--- src/app.js
+++ src/app.js
@@ -16,26 +16,21 @@ const {
 const generatedNums = numberGenerator();
 
 const getNumber = (userInput) => {
-  if (userInput.length !== NUMBER_LENGTH) {
-    terminal.question(
+  if (userInput.length !== NUMBER_LENGTH || (REGEXP).test(userInput)) {
+    return terminal.question(
       INPUT_MESSAGE.enterCorrectNumber,
       num => getNumber(num)
     );
-  } else if ((REGEXP).test(userInput)) {
-    terminal.question(
-      INPUT_MESSAGE.enterCorrectNumber,
-      num => getNumber(num)
-    );
-  } else {
-    const message = output(calculator(generatedNums, userInput));
+  }
 
-    if (message === OUTPUT_MESSAGE.win) {
-      console.log(message);
-      terminal.close();
-    } else {
-      console.log(message);
-      startGame();
-    }
+  const message = output(calculator(generatedNums, userInput));
+
+  if (message === OUTPUT_MESSAGE.win) {
+    console.log(message);
+    terminal.close();
+  } else {
+    console.log(message);
+    startGame();
   }
 };
 
