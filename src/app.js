d05062b857807036d81ab9b3f3a0743353cd9ff8
Andrzej Pietkiewicz <pietan@me.com>
2023-10-15 19:58:18 +0200

    no-console added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-/* esling-ignore no-console.log */
-"use strict";
+/* eslint-disable no-console */
+'use strict';
 
-const { numGenerator } = require("./numGenerator.js");
-const readline = require("readline");
+const { numGenerator } = require('./numGenerator.js');
+const readline = require('readline');
 
 const digitsToGuess = numGenerator();
 
@@ -10,12 +10,14 @@ const termianl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 console.clear();
+
 const ask = () => {
   let bull = 0;
   let cow = 0;
 
-  termianl.question("Guess a 4 digit number: ", (num) => {
+  termianl.question('Guess a 4 digit number: ', (num) => {
     for (let i = 0; i < 4; i++) {
       if (num[i] === digitsToGuess[i]) {
         bull++;
@@ -26,12 +28,12 @@ const ask = () => {
       }
     }
     cow -= bull;
-    console.log("bull: ", bull, ", cow: ", cow);
+    console.log('bull: ', bull, ', cow: ', cow);
 
     if (bull !== 4) {
       ask();
     } else {
-      console.log("You won! Number is:", digitsToGuess, "\n");
+      console.log('You won! Number is:', digitsToGuess, '\n');
       termianl.close();
     }
   });
