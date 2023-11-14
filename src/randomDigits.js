8e0fd0ead6fda1314a4414cd3e0218965b1b7f13
diamat1997 <diamat1997@gmail.com>
2023-11-14 15:01:45 +0200

    add task solution

---
+++ src/randomDigits.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const getRandomNumber = () => {
+  const result = [];
+  const kitDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (let i = 0; i < 4; i++) {
+    const indexDidit = Math.floor(Math.random() * kitDigits.length);
+
+    result.push(kitDigits.splice(indexDidit, 1));
+  }
+
+  return result.join('');
+};
+
+module.exports = {
+  getRandomNumber,
+};
