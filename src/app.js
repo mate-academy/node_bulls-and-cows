c9309192e8a76ce04d90df2427d385ae683f3b99
Helen Tsyhypalo <lena.tsigipalo@gmail.com>
2023-11-26 18:41:21 +0100

    add game solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const readline = require('readline');
+const { checker } = require('./digitsChecker');
+
+const terminal = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
+
+const game = () => {
+  terminal.question('Enter 4 different digits', (digits) => {
+    const answer = checker(digits);
+
+    console.log(answer);
+
+    if (answer === 'bullbullbullbull') {
+      terminal.close();
+    } else {
+      game();
+    }
+  });
+};
+
+game();
