1269cd1fd5e31c63068c12596d1ebc92d7ce3f70
YevhenKharko <yevhenkharko@gmail.com>
2023-11-28 14:05:09 +0200

    fixed

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ const calculator = require('./calculator');
     const answer = await prompt('Enter 4 digits');
     const result = calculator(num, answer);
 
+    if (answer.length !== 4) {
+      console.log('You need to enter exact 4 digits');
+      break;
+    }
+
     if (num === +answer) {
       console.log('You won!');
       break;
--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,16 +1,7 @@
 'use strict';
 
-function randomNum() {
-  const digits = Array.from({ length: 10 }, (_, i) => i);
-  let result = '';
-
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = Math.floor(Math.random() * digits.length);
-
-    result += digits.splice(randomIndex, 1)[0];
-  }
-
-  return parseInt(result, 10);
-};
+const randomNum = () => parseInt([...Array(4)]
+  .map(() => Math
+    .floor(Math.random() * 10)).join(''), 10);
 
 module.exports = randomNum;
