72a565ddb555e0311f1038d7d7565f901d775003
Yehor Korablin <yegor_ka2004@ukr.net>
2023-12-29 00:43:03 +0200

    Added loop logic

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 'use strict';
 
+/* eslint-disable no-console */
+
+const readline = require('readline');
 const countBulls = require('./modules/countBulls');
 const countCows = require('./modules/countCows');
 const generateNum = require('./modules/generateNum');
 
-const num = generateNum(1000, 9999);
-const input = +process.argv[2];
-const bulls = countBulls(num, input);
-const cows = countCows(num, input);
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-/* eslint-disable no-console */
-console.log(`Bulls: ${bulls}`);
-console.log(`Cows: ${cows}`);
+const num = generateNum(1000, 9999);
+let isFound = false;
+
+terminal.question('Type 4-digit number: ', (input) => {
+  do {
+    if (+input === num) {
+      isFound = true;
+      console.log('Yeah, you win!');
+    } else {
+      const bulls = countBulls(num, +input);
+      const cows = countCows(num, +input);
+
+      console.log(`Bulls: ${bulls}`);
+      console.log(`Cows: ${cows}`);
+    }
+  } while (!isFound);
+
+  terminal.close();
+});
