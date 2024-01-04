d3f97ed883be4d3b10ac9087e93cbe285d02ded5
Pavlo Khashchevskyi <92092584+Pavlo-Khashchevskyi@users.noreply.github.com>
2024-01-04 18:04:37 +0200

    updated fun calculatingBullsAndCows

--- src/modules/calculatingBullsAndCows.js
+++ src/modules/calculatingBullsAndCows.js
@@ -2,14 +2,17 @@ const calculatingBullsAndCows = (userNumber, randomNumber) => {
   let bulls = 0;
   let cows = 0;
 
+  const arr = [];
+
   userNumber.split('').forEach((_, i) => {
     if (userNumber[i] === randomNumber[i]) {
       bulls += 1;
+      arr.push(userNumber[i]);
     }
   });
 
   userNumber.split('').forEach((el) => {
-    if (randomNumber.includes(el)) {
+    if (randomNumber.includes(el) && !arr.includes(el)) {
       cows += 1;
     }
   });
