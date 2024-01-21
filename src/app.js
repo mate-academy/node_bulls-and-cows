cdbfe3d85aeb24167504c400c43bedababa7d509
Adrian <adrian.krause.ak@gmail.com>
2024-01-21 15:29:28 +0100

    Implemented bulls&cows game.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// Write your code here
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const readline = require('node:readline');
+
+const bcGame = () => {
+  const generatedNum = generateRandomNumber();
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const askUser = () => {
+    rl.question('Write a 4 digit number: ', (answer) => {
+      if (checkIsValidUserInput(answer)) {
+        const result = getBullsAndCows(+answer, generatedNum);
+
+        if (result.bulls === 4) {
+          rl.close();
+        } else {
+          askUser();
+        }
+      } else {
+        askUser();
+      }
+    });
+  };
+
+  askUser();
+};
+
+bcGame();
