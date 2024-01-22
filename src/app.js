2f3b739a9fb4838ae360aba5ddbac10c66ed5cb2
Mirosław Wandyk <barosz1111@gmail.com>
2024-01-22 16:04:14 +0100

    bulls and cows

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const { calculate } = require('./modules/calculate');
+
+const playGame = async() => {
+  let result = {
+    bulls: 0,
+    cows: 0,
+  };
+
+  const generatedNumber = generateRandomNumber();
+
+  while (result.bulls !== 4) {
+    const userNumber = await getBullsAndCows();
+
+    result = calculate(generatedNumber, userNumber);
+    console.log(result);
+    console.log('\n');
+  }
+
+  if (result.bulls === 4) {
+    console.log('\x1b[31m%s\x1b[0m', 'YOU WON!!!');
+  }
+};
+
+playGame();
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,8 +7,16 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
-}
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  digits.sort(() => {
+    return 0.5 - Math.random();
+  });
+
+  const uniqueFourDigitNumber = digits.slice(0, 4).join('');
+
+  return uniqueFourDigitNumber;
+};
 
 module.exports = {
   generateRandomNumber,
