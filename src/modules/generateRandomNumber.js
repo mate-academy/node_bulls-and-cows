89e8d3ccd89159edfcb1f696b786d9ac2ac27276
Antonio S <soi4.toxa@ukr.net>
2024-01-24 16:56:02 +0200

    Implemented game

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,21 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const numberLength = 4;
+  const firstNum = Math.floor(Math.random() * 9 + 1);
+  const numbers = [firstNum];
+
+  while (numbers.length < numberLength) {
+    const newNumber = Math.floor(Math.random() * 10);
+
+    if (numbers.includes(newNumber)) {
+      continue;
+    }
+
+    numbers.push(newNumber);
+  }
+
+  return +numbers.join('');
 }
 
 module.exports = {
