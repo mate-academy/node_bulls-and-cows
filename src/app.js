2881a859b6fc2f69c3e13cf38c13b9b0af6f0b2b
Marcin Ligmann <marcinligmann2@gmail.com>
2024-01-25 16:15:22 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -2,37 +2,39 @@
 /* eslint-disable max-len */
 'use strict';
 
-const generateRandomNumber = require('./modules/generateRandomNumber');
-const getBullsAndCows = require('./modules/generateRandomNumber');
+const { generateRandomNumber } = require('./modules/generateRandomNumber.js');
+const { getBullsAndCows } = require('./modules/getBullsAndCows.js');
 
 const readline = require('readline');
-const checkIsValidUserInput = require('./modules/checkIsValidUserInput');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput.js');
 
 const terminal = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const randomNumber = generateRandomNumber();
+const randNumber = generateRandomNumber();
 
-const game = () => {
+const game = (randomNumber) => {
   terminal.question('guess the number from 1234 to 9876 composed of four different numbers', (number) => {
-    if (checkIsValidUserInput(Number(number))) {
-      const { bulls, cow } = getBullsAndCows(number.toString(), randomNumber.toString());
+    const isNumberCorrect = checkIsValidUserInput(Number(number));
 
-      console.log(`You got ${bulls} bulls and ${cow}, try again!`);
-
-      if (bulls === 4) {
-        console.log(`Congratulations! You did the impossible and guessed the number!`);
-        terminal.close();
-      } else {
-        game();
-      }
-    } else {
+    if (!isNumberCorrect) {
       console.log('the number you entered is incorrect. It must be between 1234 and 9876, the numbers cannot be repeated. Try again!');
+      game();
+    }
+
+    const { bulls, cow } = getBullsAndCows(number.toString(), randomNumber.toString());
+
+    console.log(`You got ${bulls} bulls and ${cow}, try again!`);
+
+    if (bulls === 4) {
+      console.log(`Congratulations! You did the impossible and guessed the number!`);
+      terminal.close();
+    } else {
       game();
     }
   });
 };
 
-game();
+game(randNumber);
