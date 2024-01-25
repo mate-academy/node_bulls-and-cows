913b243642c9b24ece08eff54fbcddff66d91a6e
Nastia Yashnieva <57439255+Yashkindzhus@users.noreply.github.com>
2024-01-25 22:04:55 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// Write your code here
+const readline = require.resolve('readline');
+const { checkIsValidUserInput }
+  = require.resolve('./modules/checkIsValidUserInput.js');
+const { generateRandomNumber }
+  = require.resolve('./modules/generateRandomNumber.js');
+const { getBullsAndCows } = require.resolve('./modules/getBullsAndCows.js');
+
+const randomNumber = generateRandomNumber();
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function playBullsAndCows() {
+  terminal.question('Guess number with 4 different digits: ', (userAnswer) => {
+    if (checkIsValidUserInput(userAnswer)) {
+      const result = getBullsAndCows(userAnswer, randomNumber);
+
+      terminal.write(`Bulls: ${result.bulls}, cows: ${result.cows}`);
+
+      if (result.bulls === 4) {
+        terminal.write('You won!');
+        terminal.close();
+      } else {
+        playBullsAndCows();
+      }
+    } else {
+      terminal.write('Please enter valid 4 digit number!');
+      playBullsAndCows();
+    }
+  });
+}
+
+playBullsAndCows();
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,20 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  let number = '';
+  const digits = Array.from({ length: 9 }, (_, i) => i);
+
+  for (let i = 0; i < 4; i++) {
+    const randomDigit = Math.ceil(Math.random() * digits.length);
+
+    if (!number.includes(randomDigit)) {
+      number += randomDigit;
+    } else {
+      i--;
+    }
+  }
+
+  return +number;
 }
 
 module.exports = {
