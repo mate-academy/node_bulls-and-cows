af6acceef4b83ee987b17a9e5ba19eb0e409a9d6
Dmytro Yarechuk <retrodn14@gmail.com>
2024-03-04 17:44:28 +0200

    add task solution w/ terminal game

--- src/modules/getBullsAndCows.js
+++ src/modules/getBullsAndCows.js
@@ -1,19 +1,30 @@
 'use strict';
 
-/**
- * Calculate the number of bulls and cows for a given user input.
- * Bulls are digits that are in the correct position.
- * Cows are digits that are in the wrong position.
- * Assume that the user input and the number to guess
- * are always 4-digit numbers.
- *
- * @param {number} userInput - The user input
- * @param {number} numberToGuess - The number to guess
- * @return {object} An object containing the number of bulls and cows.
- * Example: { bulls: 1, cows: 2 }
- */
 function getBullsAndCows(userInput, numberToGuess) {
-  /* Write your code here */
+  const bullsAndCows = {
+    bulls: 0,
+    cows: 0,
+  };
+
+  const inputStr = `${userInput}`;
+  const numberToGuessStr = `${numberToGuess}`;
+
+  for (let i = 0; i < inputStr.length; i++) {
+    if (
+      numberToGuessStr.includes(inputStr[i])
+      && inputStr.indexOf(inputStr[i]) !== numberToGuessStr.indexOf(inputStr[i])
+    ) {
+      bullsAndCows.cows++;
+    }
+
+    if (
+      inputStr.indexOf(inputStr[i]) === numberToGuessStr.indexOf(inputStr[i])
+    ) {
+      bullsAndCows.bulls++;
+    }
+  }
+
+  return bullsAndCows;
 }
 
 module.exports = {
