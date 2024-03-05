b071a2a6b3cdbecf97d54a887c341e8ffb21c786
Sergey Kirichenko <s.kirichenko@m-dev.org>
2024-03-05 19:51:57 +0200

    testing v0.2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-'use strict';
+/* eslint-disable no-console */
+"use strict";
 
-const readline = require('readline');
-const { generateRandomNumber } = require('./modules/generateRandomNumber');
-const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
-const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const readline = require("readline");
+const { generateRandomNumber } = require("./modules/generateRandomNumber");
+const { checkIsValidUserInput } = require("./modules/checkIsValidUserInput");
+const { getBullsAndCows } = require("./modules/getBullsAndCows");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,10 +16,10 @@ const gameNumber = generateRandomNumber();
 console.log(gameNumber);
 
 function startGame() {
-  rl.question('Guess number: ', (guess) => {
+  rl.question("Guess number: ", (guess) => {
     if (checkIsValidUserInput(guess)) {
       if (parseInt(guess) === gameNumber) {
-        console.log('Winner');
+        console.log("Winner");
         rl.close();
       } else {
         const answer = getBullsAndCows(guess, gameNumber);
@@ -27,7 +28,7 @@ function startGame() {
         startGame();
       }
     } else {
-      console.log('Invalid number');
+      console.log("Invalid number");
       startGame();
     }
   });
