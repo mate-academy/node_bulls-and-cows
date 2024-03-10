c242750229a4f972064dfd6391699c3a2d62b6f0
Marek Maciejewski <marekmaciejewski1982@gmail.com>
2024-03-10 22:36:47 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,45 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function bullsAndCows() {
+  const numberToGuess = generateRandomNumber();
+
+  function guessTheNumber() {
+    rl.question('Guess the 4-digit number: ', (userInput) => {
+      if (!checkIsValidUserInput(userInput)) {
+        console.log(
+          'Invalid input. '
+          + 'Please enter a 4-digit number that '
+          + 'does not contain any duplicate digits.'
+        );
+
+        return guessTheNumber();
+      }
+
+      const result = getBullsAndCows(+userInput, numberToGuess);
+
+      console.log(result);
+
+      if (result.bulls === 4) {
+        console.log('Congratulations! You guessed the number!');
+        rl.close();
+      } else {
+        guessTheNumber();
+      }
+    });
+  }
+
+  guessTheNumber();
+}
+
+bullsAndCows();
