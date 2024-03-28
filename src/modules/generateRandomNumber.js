32eba4bf17701b6e1348d87a6d0124ed21eb981a
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-03-28 15:20:35 +0100

    solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,19 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  let randomNum = '';
+
+  randomNum += Math.floor(Math.random() * 9) + 1;
+
+  while (randomNum.length !== 4) {
+    const num = Math.floor(Math.random() * 10);
+
+    if (!randomNum.includes(num)) {
+      randomNum += num;
+    }
+  }
+
+  return +randomNum;
 }
 
 module.exports = {
