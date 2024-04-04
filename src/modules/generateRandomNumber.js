c117be690f03e15dc99517e2b3e72b67c13a70a8
HolyRaveOS <kitlaruk1995@gmail.com>
2024-04-04 17:35:15 +0300

    made correction

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,19 +7,22 @@
  * @return {number} A random 4-digit number
  */
 
-const getRandomNum = () => {
-  return Math.floor(Math.random() * 9000) + 1000;
+const generateRandomNumber = () => {
+  const digits = '0123456789';
+  let randomNumber = '';
+
+  randomNumber += digits[Math.floor(Math.random() * 9) + 1];
+
+  while (randomNumber.length < 4) {
+    const randomNum = Math.floor(Math.random() * 10);
+
+    if (!randomNumber.includes(randomNum)) {
+      randomNumber += randomNum;
+    }
+  }
+
+  return parseInt(randomNumber);
 };
-
-function generateRandomNumber() {
-  let res;
-
-  do {
-    res = getRandomNum();
-  } while (new Set(String(res).split('')).size !== 4);
-
-  return res;
-}
 
 module.exports = {
   generateRandomNumber,
