d8af29fca9b009fca18e82cf8e212cd92a53c0e9
HolyRaveOS <kitlaruk1995@gmail.com>
2024-04-04 17:38:59 +0300

    made correction

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -8,10 +8,9 @@
  */
 
 const generateRandomNumber = () => {
-  const digits = '0123456789';
   let randomNumber = '';
 
-  randomNumber += digits[Math.floor(Math.random() * 9) + 1];
+  randomNumber += Math.floor(Math.random() * 9) + 1;
 
   while (randomNumber.length < 4) {
     const randomNum = Math.floor(Math.random() * 10);
