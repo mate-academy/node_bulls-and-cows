fc05e00a6575e2c927da0371aa18585ed113b95a
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-04-09 15:52:15 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const terminal = readline.createInterface(process.stdin, process.stdout);
+const randomNumber = generateRandomNumber();
+
+function askQuestion() {
+  terminal.question('Enter a 4-digit number: ', (userInput) => {
+    if (checkIsValidUserInput(userInput)) {
+      const { bulls, cows } = getBullsAndCows(+userInput, randomNumber);
+
+      if (bulls === 4) {
+        console.log('Congratulations! You guessed the number!');
+        terminal.close();
+      } else {
+        console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+        askQuestion();
+      }
+    } else {
+      askQuestion();
+    }
+  });
+}
+
+askQuestion();
