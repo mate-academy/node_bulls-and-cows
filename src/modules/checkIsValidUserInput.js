f89842cba7bad37403e70640278cdcd35eebc389
Basil-Panasiuk <v.panasiuk.job@gmil.com>
2024-04-12 21:29:17 +0300

    Solution

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,28 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  if (isNaN(userInput)) {
+    return false;
+  }
+
+  if (userInput < 1000 || userInput > 9999) {
+    return false;
+  }
+
+  const digits = userInput.split('');
+
+  if (digits[0] === '0') {
+    return false;
+  }
+  digits.sort();
+
+  for (let i = 1; i < digits.length - 1; i++) {
+    if (digits[i - 1] === digits[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = {
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,19 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const digits = Array.from(Array(10).keys());
+
+  for (let i = digits.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [digits[i], digits[j]] = [digits[j], digits[i]];
+  }
+
+  const firstIndex = digits[0] === 0 ? 1 : 0;
+
+  const result = digits.slice(firstIndex, firstIndex + 4).join('');
+
+  return +result;
 }
 
 module.exports = {
