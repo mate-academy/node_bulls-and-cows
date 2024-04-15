53eef1fd6ebc44307ece3bcde6978c49137d7f8e
Anna Basenyuk <annabasenyuk@gmail.com>
2024-04-15 19:55:50 +0300

    add solution to the task

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,22 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  function random() {
+    const minNumber = 1000;
+    const maxNumber = 10000;
+
+    return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+  }
+
+  let result = random().toString();
+  let uniq = new Set(result);
+
+  while (result[0] === '0' || uniq.size !== 4) {
+    result = random().toString();
+    uniq = new Set(result);
+  }
+
+  return parseInt(result);
 }
 
 module.exports = {
