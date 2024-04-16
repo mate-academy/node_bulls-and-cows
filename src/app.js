83150fd61f1148cb9853d84e4288e1ed01aacbcd
Anna Basenyuk <annabasenyuk@gmail.com>
2024-04-16 17:57:40 +0300

    add app.js game

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const truesNumber = generateRandomNumber();
+
+const game = () => {
+  terminal.question('Please enter a four-digit number: ', (userNumber) => {
+    if (checkIsValidUserInput(userNumber)) {
+      const { bulls, cows } = getBullsAndCows(userNumber, truesNumber);
+
+      if (bulls !== 4) {
+        // eslint-disable-next-line no-console
+        console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+
+        game();
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Congratulations!!!');
+
+        terminal.close();
+      }
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Incorrect number');
+
+      game();
+    }
+  });
+};
+
+game();
