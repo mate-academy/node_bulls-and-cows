545bf9526ea8cc05b1ab8dec57945b13d855d54b
foxstudent103376 <yashopua@gmail.com>
2024-04-17 14:48:07 +0300

    add solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -1,13 +1,19 @@
 'use strict';
 
-/**
- * Generate a random 4-digit number that does not start with 0
- * and does not contain any duplicate digits.
- *
- * @return {number} A random 4-digit number
- */
 function generateRandomNumber() {
-  /* Write your code here */
+  let randomNumber = '';
+
+  randomNumber += Math.floor(Math.random() * 9) + 1;
+
+  while (randomNumber.length < 4) {
+    const digit = Math.floor(Math.random() * 10);
+
+    if (!randomNumber.includes(digit.toString())) {
+      randomNumber += digit;
+    }
+  }
+
+  return +randomNumber;
 }
 
 module.exports = {
--- src/modules/getBullsAndCows.js
+++ src/modules/getBullsAndCows.js
@@ -1,19 +1,21 @@
 'use strict';
 
-/**
- * Calculate the number of bulls and cows for a given user input.
- * Bulls are digits that are in the correct position.
- * Cows are digits that are in the wrong position.
- * Assume that the user input and the number to guess
- * are always 4-digit numbers.
- *
- * @param {number} userInput - The user input
- * @param {number} numberToGuess - The number to guess
- * @return {object} An object containing the number of bulls and cows.
- * Example: { bulls: 1, cows: 2 }
- */
 function getBullsAndCows(userInput, numberToGuess) {
-  /* Write your code here */
+  let bulls = 0;
+  let cows = 0;
+
+  const userArr = userInput.toString().split('');
+  const randomArr = numberToGuess.toString().split('');
+
+  for (let i = 0; i < 4; i++) {
+    if (userArr[i] === randomArr[i]) {
+      bulls++;
+    } else if (randomArr.includes(userArr[i])) {
+      cows++;
+    }
+  }
+
+  return { bulls, cows };
 }
 
 module.exports = {
