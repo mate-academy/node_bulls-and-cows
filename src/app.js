f2a082b98ee69abb9a8c1201a5f1294e83c29150
Nirvacsh <nir.vlad.zu@gmail.com>
2024-05-01 10:57:25 +0300

    hotfix

--- src/app.js
+++ src/app.js
@@ -7,30 +7,50 @@ const { getBullsAndCows } = require('./modules/getBullsAndCows');
 
 const terminal = readline.createInterface(process.stdin, process.stdout);
 const randomNum = generateRandomNumber();
+const MAX_NUMBER_OF_DIGITS = 4;
+const ENTER_A_NUMBER = `Enter a number of ${MAX_NUMBER_OF_DIGITS} different digits`;
+const MESSAGE_FOR_WIN = `You guessed all numbers. Game is over!`;
+const MESSAGE_FOR_LOSE = `You entered incorrect numbers. Please try again.\n`;
 
-function askForNumber() {
-  let isValidInput = false;
+async function askForNumber() {
+  while (true) {
+    const digits = await promptUserInput(ENTER_A_NUMBER);
 
-  while (!isValidInput) {
-    terminal.question('Enter a number of 4 different digits', (digits) => {
-      if (checkIsValidUserInput(digits)) {
-        const result = getBullsAndCows(digits, randomNum);
+    if (checkIsValidUserInput(digits)) {
+      const result = getBullsAndCows(digits, randomNum);
 
-        process.stdout.write(`Result: ${JSON.stringify(result)}\n`);
+      displayResult(result);
 
-        if (result.bulls === 4) {
-          terminal.close();
-          process.stdout.write(`You guessed all numbers. Game is over!`);
-        } else {
-          isValidInput = true;
-        }
-      } else {
-        process.stdout.write(
-          `You entered incorrect numbers. Please try again.\n`,
-        );
+      if (result.bulls === MAX_NUMBER_OF_DIGITS) {
+        displayWinMessage();
+        terminal.close();
+
+        return;
       }
-    });
+    } else {
+      displayLoseMessage();
+    }
   }
+}
+
+function promptUserInput(prompt) {
+  return new Promise((resolve) => {
+    terminal.question(prompt, (input) => {
+      resolve(input);
+    });
+  });
+}
+
+function displayResult(result) {
+  process.stdout.write(`Result: ${JSON.stringify(result)}\n`);
+}
+
+function displayWinMessage() {
+  process.stdout.write(MESSAGE_FOR_WIN);
+}
+
+function displayLoseMessage() {
+  process.stdout.write(MESSAGE_FOR_LOSE);
 }
 
 askForNumber();
