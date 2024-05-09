f941c4b07d376cbb78e7c6df0e591435855e3858
Olena Shapkun <shapkunkh@gmail.com>
2024-05-09 18:24:58 +0300

    game solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const terminal = readline.createInterface(process.stdin, process.stdout);
+
+const numberToGuess = generateRandomNumber();
+
+function startGame() {
+  terminal.question(
+    'Enter a 4-digit number with no repeating digits: ',
+    (userInput) => {
+      if (!checkIsValidUserInput(userInput)) {
+        console.log(
+          'Invalid input.',
+          'Please enter a 4-digit number with no repeating',
+          'and not starting with 0',
+        );
+
+        return startGame();
+      }
+
+      const result = getBullsAndCows(parseInt(userInput), numberToGuess);
+
+      if (result.bulls === 4) {
+        console.log('Congratulations! You guessed the number!');
+        return terminal.close();
+      }
+
+      console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+      startGame();
+    },
+  );
+}
+
+startGame();
