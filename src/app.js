1f21e9c40ad8386859ecf6de789e35678f31022e
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-06-24 19:58:19 +0300

    create the game

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// Write your code here
+import readline from 'readline';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput';
+import { generateRandomNumber } from './modules/generateRandomNumber';
+import { getBullsAndCows } from './modules/getBullsAndCows';
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const numberToGuess = generateRandomNumber();
+
+terminal.question('Enter a 4-digit number to guess: ', (userInput) => {
+  if (checkIsValidUserInput(userInput)) {
+    getBullsAndCows(+userInput, numberToGuess);
+  } else {
+    promptUser();
+  }
+
+  terminal.close();
+});
+
+function promptUser() {
+  terminal.question('Enter a 4-digit number to guess: ', (userInput) => {
+    if (checkIsValidUserInput(userInput)) {
+      getBullsAndCows(+userInput, numberToGuess);
+      terminal.close();
+    } else {
+      promptUser();
+    }
+  });
+}
