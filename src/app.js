b815a21c1b4758f0d583714a1b992c925df6eaa9
katya havriuk <0303gavkat@gmail.com>
2024-06-24 20:52:46 -0600

    task solved

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// Write your code here
+const readline = require('node:readline');
+
+const { getBullsAndCows } = require('./modules/getBullsAndCows').default;
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const output = (text) => {
+  rl.output.write(`${text}\n`);
+};
+const randomNumber = generateRandomNumber().toString();
+
+const play = () => {
+  rl.question('Guess a number: ', (number) => {
+    if (!checkIsValidUserInput(number)) {
+      output('Wrong input, enter 4 digitnumber with unique numbers');
+      play();
+    } else {
+      const { bulls, cows } = getBullsAndCows(number, randomNumber);
+
+      output(`Bulls: ${bulls}, Cows: ${cows}`);
+
+      if (bulls === 4) {
+        output('You won!');
+        rl.close();
+      } else {
+        output('Try again');
+        play();
+      }
+    }
+  });
+};
+
+play();
