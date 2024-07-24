5e7c675ec07977c271b786bf399e5bbff774ce3b
Александр <0999353523ak@gmail.com>
2024-07-24 12:41:57 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const game = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const numberToGuess = generateRandomNumber().toString();
+
+function newGame() {
+  game.question(`Enter your number  `, (userInput) => {
+    if (!checkIsValidUserInput(userInput)) {
+      console.log('Enter correct number');
+      newGame();
+    } else if (userInput === numberToGuess) {
+      console.log('YOU WIN!!!');
+      game.close();
+    } else {
+      console.log(getBullsAndCows(userInput, numberToGuess));
+      newGame();
+    }
+  });
+}
+
+newGame();
+
+module.exports = {
+  newGame,
+};
