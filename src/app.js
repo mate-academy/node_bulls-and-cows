ced33675f12b982cc4bcbcb6bdc78f837cf95b6d
Victor Hrygorash <viktor12041991@i.ua>
2024-08-12 12:01:36 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const numberToGuess = generateRandomNumber().toString();
+
+function questionForUser() {
+  terminal.question('Enter your guess: ', (userInput) => {
+    if (!checkIsValidUserInput(userInput)) {
+      console.log(
+        'Invalid input. Please enter a 4-digit number without duplicates.',
+      );
+      questionForUser();
+
+      return;
+    }
+
+    const result = getBullsAndCows(userInput, numberToGuess);
+
+    console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+
+    if (result.bulls === 4) {
+      console.log('Congratulations! You guessed the number.');
+      terminal.close();
+    } else {
+      questionForUser();
+    }
+  });
+}
+
+questionForUser();
