365976bcfb84907838ad320e9e5b516c7fef1d2e
Anastasiia Bardyuzha <anastasiiabardyuzha@AnastasiiasMBP2.lan>
2024-09-01 15:57:21 -0500

    implemented modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,1 @@
 'use strict';
-
-// Write your code here
--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,17 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  const digitsArray = [...new Set(userInput)];
+
+  if (digitsArray.length !== 4) {
+    return false;
+  }
+
+  if (userInput[0] === '0') {
+    return false;
+  }
+
+  return !/\D/.test(userInput);
 }
 
 module.exports = {
