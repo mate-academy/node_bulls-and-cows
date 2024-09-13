077042ab4e447731baaf5f3503ef83241706222d
BodyaKuts <mkkbodya@gmail.com>
2024-09-13 11:12:42 +0300

    implemented number generation and validation

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -1,13 +1,21 @@
 'use strict';
 
-/**
- * Generate a random 4-digit number that does not start with 0
- * and does not contain any duplicate digits.
- *
- * @return {number} A random 4-digit number
- */
 function generateRandomNumber() {
-  /* Write your code here */
+  const output = [];
+  const numLen = 4;
+  let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (let i = 0; i < numLen; i++) {
+    const startIndex = +(i === 0);
+    const position =
+      Math.floor(Math.random() * (nums.length - startIndex)) + startIndex;
+    const randomNum = nums[position];
+
+    nums = nums.filter((elem) => elem !== randomNum);
+    output.push(randomNum);
+  }
+
+  return output;
 }
 
 module.exports = {
