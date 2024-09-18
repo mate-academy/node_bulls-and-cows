ebdf54c3eb9152aea050c0f7445573822d929f80
Yurii <urabelivr@gmail.com>
2024-09-18 20:44:37 +0200

    add bulls and cows game

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,22 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let resultString = '';
+
+  const firstDigitIndex = Math.floor(Math.random() * 9 + 1);
+
+  resultString += DIGITS[firstDigitIndex];
+  DIGITS.splice(firstDigitIndex, 1);
+
+  for (let i = 0; i < 3; i++) {
+    const digitIndex = Math.floor(Math.random() * DIGITS.length);
+
+    resultString += DIGITS[digitIndex];
+    DIGITS.splice(digitIndex, 1);
+  }
+
+  return +resultString;
 }
 
 module.exports = {
