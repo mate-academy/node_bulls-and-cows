847c1b461e269f324af216cc4ea19e8189f36416
Ostap Hedz <lordanvil18@gmail.com>
2024-09-25 19:13:40 +0200

    solution done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber.js');
+const { getBullsAndCows } = require('./modules/getBullsAndCows.js');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput.js');
+
+const numberToGues = generateRandomNumber();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function question() {
+  rl.question('What is your number?', (userAnswer) => {
+    if (!checkIsValidUserInput(userAnswer)) {
+      console.log('Number must include 4 unique digits');
+
+      return question();
+    }
+
+    const result = getBullsAndCows(userAnswer, numberToGues);
+
+    console.log(
+      `Your number: ${userAnswer} ||`,
+      `Random number: ${numberToGues}`,
+      result,
+    );
+    rl.close();
+  });
+}
+
+question();
