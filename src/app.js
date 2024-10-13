01f901f90d6ff96f05f70eb1dc25df6c13739e84
Diana Tuz <dianatuz12@gmail.com>
2024-10-13 15:28:10 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+
+const cowsOrBull = readline.createInterface(process.stdin, process.stdout);
+
+const playGame = () => {
+  const generatedNumbers = generateRandomNumber();
+
+  console.log(generatedNumbers);
+
+  const guess = () => {
+    cowsOrBull.question(
+      `Let's start our game! Try to guess the numbers: `,
+      (numbers) => {
+        const answer = checkIsValidUserInput(numbers);
+
+        if (!answer) {
+          console.log(
+            'Ouch! That’s not it—let’s get some valid numbers in there!',
+          );
+
+          return guess();
+        }
+
+        const result = getBullsAndCows(numbers, generatedNumbers);
+
+        if (result && result.bulls === 4) {
+          console.log('Yippee! You found all my bulls! I’m over the moon!');
+          cowsOrBull.close();
+        } else {
+          console.log(result);
+          guess();
+        }
+      },
+    );
+  };
+
+  guess();
+};
+
+playGame();
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,21 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const numbers = [];
+
+  const firstDigit = Math.floor(Math.random() * 9) + 1;
+
+  numbers.push(firstDigit);
+
+  while (numbers.length < 4) {
+    const newNumber = Math.floor(Math.random() * 10);
+
+    if (!numbers.includes(newNumber)) {
+      numbers.push(newNumber);
+    }
+  }
+
+  return Number(numbers.join(''));
 }
 
 module.exports = {
