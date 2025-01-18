e91cbb5c881a9ace6c95a6c17ab50c23be2f171c
Andrii Tkachuk <kovklad@gmail.com>
2025-01-18 15:52:12 +0100

    Solution

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,24 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  const allNums = '0123456789';
+  const arr = userInput.toString().split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    const arrSpliced = [...arr];
+
+    arrSpliced.splice(i, 1);
+
+    if (!allNums.includes(arr[i]) || arrSpliced.includes(arr[i])) {
+      return false;
+    }
+  }
+
+  if (arr[0] === '0' || arr.length !== 4) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = {
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,21 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const arr = [];
+
+  for (let i = 0; i < 4; i++) {
+    const randomDigit = Math.floor(Math.random() * 10);
+
+    if (i === 0 && randomDigit !== 0 && !arr.includes(randomDigit)) {
+      arr.unshift(randomDigit);
+    } else if (i !== 0 && !arr.includes(randomDigit)) {
+      arr.push(randomDigit);
+    } else {
+      i -= 1;
+    }
+  }
+
+  return +arr.join('');
 }
 
 module.exports = {
