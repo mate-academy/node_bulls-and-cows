f435e9422a2f514c8151b476263ed46ed827cf18
Mariia <maria.gula0307@gmail.com>
2025-01-28 23:50:35 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const numberToGuess = generateRandomNumber();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+askForNumber();
+
+function askForNumber() {
+  rl.question('\nEnter a 4-digit number: ', (userInput) => {
+    if (!checkIsValidUserInput(userInput)) {
+      console.log('Invalid input. Try again.');
+      askForNumber();
+    } else {
+      const { bulls, cows } = getBullsAndCows(Number(userInput), numberToGuess);
+
+      if (bulls === 4) {
+        console.log('Congratulations! You won!');
+        rl.close();
+      } else {
+        console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+        askForNumber();
+      }
+    }
+  });
+}
