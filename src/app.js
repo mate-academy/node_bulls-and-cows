ce4d59c9531c7548a89d5cdb5ec02ec6803d9a27
Vitor <vitorhugovieirabraga@gmail.com>
2025-02-04 18:28:04 -0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules//getBullsAndCows');
+
+const num = generateRandomNumber();
+
+const trm = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askGuess() {
+  trm.question('Enter the number', (number) => {
+    if (checkIsValidUserInput(number)) {
+      const result = getBullsAndCows(parseInt(number), num);
+
+      if (result.bulls === 4) {
+        trm.close();
+      } else {
+        askGuess();
+      }
+    } else {
+      askGuess();
+    }
+  });
+}
+
+askGuess();
