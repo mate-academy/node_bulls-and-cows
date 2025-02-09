571ab248cfb192aa8da4264ad160860502de94cd
Vyacheslav Kokayko <vkokayko@gmail.com>
2025-02-09 21:03:02 +0100

    node_bulls-and-cows

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,32 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  if (isNaN(userInput)) {
+    // eslint-disable-next-line no-console
+    console.log('Invalid input. Please enter a 4-digit number.');
+
+    return false;
+  }
+
+  const numbers = String(userInput)
+    .split('')
+    .map((n) => Number(n));
+
+  if (numbers[0] === 0 || numbers.length !== 4) {
+    // eslint-disable-next-line no-console
+    console.log('Error. Enter a 4-digit number that doesnt start with 0.');
+
+    return false;
+  }
+
+  if (new Set(numbers).size !== numbers.length) {
+    // eslint-disable-next-line no-console
+    console.log('Error. Enter a 4-digit number that has only unique digits.');
+
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = {
