d82baeebb6cbf9de00c36c18aad86cbdffca067d
Tanya Baletska <t.baletska@gmail.com>
2025-02-12 17:04:01 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const generate = require('./modules/generateRandomNumber');
+const score = require('./modules/getBullsAndCows');
+const valid = require('./modules/checkIsValidUserInput');
+
+const secret = generate();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const ask = () =>
+  rl.question('Твій варіант: ', (guess) => {
+    if (!valid(guess)) {
+      return ask();
+    }
+
+    const { bulls } = score(secret, guess);
+
+    if (bulls === 4) {
+      rl.close();
+    } else {
+      ask();
+    }
+  });
+
+ask();
