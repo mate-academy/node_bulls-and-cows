c24b28050686d2002f2f781cd9b7ed8fe3b21b04
Svitlana <svitlana.gaiduk@gmail.com>
2025-02-18 14:31:57 +0200

    add task solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,17 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const randomNumberArr = [Math.floor(Math.random() * 9 + 1)];
+
+  while (randomNumberArr.length < 4) {
+    const randNum = Math.floor(Math.random() * 10);
+
+    if (!randomNumberArr.includes(randNum)) {
+      randomNumberArr.push(randNum);
+    }
+  }
+
+  return +randomNumberArr.join('');
 }
 
 module.exports = {
