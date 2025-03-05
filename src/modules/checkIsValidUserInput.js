94b9f6ba43331bcc23f57f6823232e7a30109bbd
tiblazy <hauntedapply@gmail.com>
2025-03-05 11:15:37 -0300

    validUserInput

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,15 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  return !!(
+    Number(userInput[0]) === 0 ||
+    userInput.length !== 4 ||
+    userInput
+      .split('')
+      .map(Number)
+      .sort((a, b) => a - b)
+      .find((equal, index) => equal === Number(userInput[index + 1]))
+  );
 }
 
 module.exports = {
