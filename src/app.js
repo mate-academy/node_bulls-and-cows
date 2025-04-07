433fcf65fc89a7d8eb1aea87ee70deb04ef044ac
Oleksandr Rad <clavigo365@gmail.com>
2025-04-07 14:27:21 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const randomNumber = generateRandomNumber();
+
+// eslint-disable-next-line no-console
+// console.log(randomNumber);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askUser() {
+  rl.question('Guess (type number of 4 different digits): ', (userInput) => {
+    const isValid = checkIsValidUserInput(userInput);
+
+    if (!isValid) {
+      // eslint-disable-next-line no-console
+      console.log('Please enter a valid number');
+
+      return askUser();
+    }
+
+    const res = getBullsAndCows(userInput, randomNumber.toString());
+
+    // eslint-disable-next-line no-console
+    console.log(`${res.bulls} bulls, ${res.cows} cows.`);
+
+    if (res.bulls === 4) {
+      // eslint-disable-next-line no-console
+      console.log('🎉 You guessed the number!');
+
+      rl.close();
+    } else {
+      askUser();
+    }
+  });
+}
+
+askUser();
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,26 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  // Fisher–Yates shuffle
+  for (let i = digits.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [digits[i], digits[j]] = [digits[j], digits[i]];
+  }
+
+  // Ensure the first digit is not 0
+  if (digits[0] === '0') {
+    for (let i = 1; i < digits.length; i++) {
+      if (digits[i] !== '0') {
+        [digits[0], digits[i]] = [digits[i], digits[0]];
+        break;
+      }
+    }
+  }
+
+  return parseInt(digits.slice(0, 4).join(''), 10);
 }
 
 module.exports = {
