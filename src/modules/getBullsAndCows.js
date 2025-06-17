83fcc89cc9b1de1bd4ed1bf77313ba18a0d8c0c0
Heny <jackbalageru@yahoo.com>
2025-06-17 15:06:18 +0300

    Solution

--- src/modules/getBullsAndCows.js
+++ src/modules/getBullsAndCows.js
@@ -14,6 +14,27 @@
  */
 function getBullsAndCows(userInput, numberToGuess) {
   /* Write your code here */
+  const inputStr = userInput.toString();
+  const guessStr = numberToGuess.toString();
+
+  let bulls = 0;
+  let cows = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (inputStr[i] === guessStr[i]) {
+      bulls++;
+    }
+  }
+
+  const inputDigits = inputStr.split('');
+  const guessDigits = guessStr.split('');
+  const totalMatches = inputDigits.reduce((acc, digit) => {
+    return acc + (guessDigits.includes(digit) ? 1 : 0);
+  }, 0);
+
+  cows = totalMatches - bulls;
+
+  return { bulls, cows };
 }
 
 module.exports = {
