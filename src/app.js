361810d118f0f0aad7a60ff6ea9bc0c3e2f1a95e
TetianaHodis1308 <hodistetianajob@ukr.net>
2025-08-21 17:41:22 +0300

    added solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('node:readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function startGame() {
+  const randomNumber = generateRandomNumber();
+
+  function ask() {
+    rl.question(
+      'Welcome to the game Bulls and cows! Please enter your number:',
+      (personNumber) => {
+        if (!checkIsValidUserInput(personNumber)) {
+          console.log('Invalid number. Please enter a valid 4-digit number.');
+          ask();
+        } else {
+          const result = getBullsAndCows(personNumber, randomNumber);
+
+          if (result.bulls === 4) {
+            console.log('Congratulations! You guessed the number!');
+            rl.close();
+          } else {
+            const bulls = result.bulls;
+            const cows = result.cows;
+
+            console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+            ask();
+          }
+        }
+      },
+    );
+  }
+  ask();
+}
+startGame();
+
+module.exports = {
+  startGame,
+};
