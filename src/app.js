2504f56450f4d3a3d2a00e97807d09fa57629527
Makhmud <bmm0065@gmail.com>
2025-09-08 22:41:43 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
+/* eslint-disable no-console */
 'use strict';
 
 // Write your code here
+const readline = require('readline');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const secretNumber = generateRandomNumber();
+
+console.log('Добро пожаловать в игру "Быки и Коровы"! 🎯');
+console.log('Я загадал 4-значное число. Попробуй угадать!');
+
+function askUser() {
+  rl.question('Введи 4-значное число: ', (answer) => {
+    if (!checkIsValidUserInput(answer)) {
+      console.log(
+        '❌ Неверный ввод! Введи 4 уникальные цифры, первая не может быть 0.',
+      );
+
+      return askUser();
+    }
+
+    const { bulls, cows } = getBullsAndCows(Number(answer), secretNumber);
+
+    console.log(`Быки: ${bulls}, Коровы: ${cows}`);
+
+    if (bulls === 4) {
+      console.log(`🎉 Поздравляю! Ты угадал число ${secretNumber}!`);
+      rl.close();
+    } else {
+      askUser();
+    }
+  });
+}
+
+askUser();
