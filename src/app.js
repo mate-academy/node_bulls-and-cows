a5258a4f95d914e2a3ed4523998fbee3a14aa797
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-09-18 16:38:41 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// Write your code here
+const readline = require('node:readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const numberToGuess = generateRandomNumber();
+
+const askQuestion = () => {
+  rl.question('Guess the four-digit number: ', (userInput) => {
+    if (checkIsValidUserInput(userInput)) {
+      const bullsAndCows = getBullsAndCows(userInput, numberToGuess);
+
+      if (bullsAndCows.bulls === 4) {
+        rl.write(`🎉 You guessed it! Number was ${numberToGuess}\n`);
+        rl.close();
+      } else {
+        rl.write(`Bulls: ${bullsAndCows.bulls}, Cows: ${bullsAndCows.cows}\n`);
+        askQuestion();
+      }
+    } else {
+      rl.write('❌ Invalid input. Try again!\n');
+      askQuestion();
+    }
+  });
+};
+
+askQuestion();
--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,16 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  if (
+    userInput.length === 4 &&
+    userInput[0] !== '0' &&
+    Array.from(new Set(userInput.split(''))).length === userInput.length &&
+    userInput.split('').every((num) => Number(num) || num === '0')
+  ) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = {
