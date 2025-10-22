36c7e2c0e4d978f4e54bb0e64fee13cd4fdd864c
Oksana Kuziv <oksanakuziv25@gmail.com>
2025-10-22 16:41:27 +0200

    task solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('node:readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function startGame() {
+  const randomNumber = generateRandomNumber();
+
+  function ask() {
+    terminal.question(
+      'Welcome to the game! Please enter your number: ',
+      (personNumber) => {
+        if (!checkIsValidUserInput(personNumber)) {
+          console.log('Invalid number. Please enter a valid 4-digit number.');
+          ask();
+        } else {
+          const result = getBullsAndCows(personNumber, randomNumber);
+
+          if (result.bulls === 4) {
+            console.log('Congratulations! You guessed the number!');
+            terminal.close();
+          } else {
+            const bulls = result.bulls;
+            const cows = result.cows;
+
+            console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+            ask();
+          }
+        }
+      },
+    );
+  }
+  ask();
+}
+startGame();
--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,18 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  const unique = new Set(userInput);
+
+  if (
+    userInput.length !== 4 ||
+    isNaN(userInput) ||
+    userInput[0] === '0' ||
+    unique.size !== 4
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = {
