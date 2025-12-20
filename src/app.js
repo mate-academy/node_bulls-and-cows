422e2cdc2b6c6bdcb32aad001ce59a44067cdc70
Vouchyk <user@users-MacBook-Air.local>
2025-12-20 11:21:55 +0000

    Tasks solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,50 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('../src/modules/generateRandomNumber');
+const { getBullsAndCows } = require('../src/modules/getBullsAndCows');
+const {
+  checkIsValidUserInput,
+} = require('../src/modules/checkIsValidUserInput');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const numberToGuess = [
+  generateRandomNumber(),
+  generateRandomNumber(),
+  generateRandomNumber(),
+  generateRandomNumber(),
+];
+
+console.log(numberToGuess);
+
+function ask() {
+  rl.question('Введи число: ', (answer) => {
+    if (!checkIsValidUserInput(answer)) {
+      console.log('Ти ввів неправильне число');
+
+      return ask();
+    }
+
+    const userNumber = answer.split('').map(Number);
+
+    const { bulls, cows } = getBullsAndCows(userNumber, numberToGuess);
+
+    console.log(`Bulls: ${bulls}, Cows: ${cows}`);
+
+    if (bulls === 4) {
+      console.log('Ти виграв 🎉');
+      rl.close();
+
+      return;
+    }
+
+    ask();
+  });
+}
+
+ask();
