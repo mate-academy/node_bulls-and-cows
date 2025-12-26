c64d4995e8644bc7e85abaf6a98baec16ce883bf
Kyrylo Golikov <pipecc1985@gmail.com>
2025-12-26 16:52:34 +0100

    solution

--- src/modules/getBullsAndCows.js
+++ src/modules/getBullsAndCows.js
@@ -12,8 +12,23 @@
  * @return {object} An object containing the number of bulls and cows.
  * Example: { bulls: 1, cows: 2 }
  */
+
 function getBullsAndCows(userInput, numberToGuess) {
-  /* Write your code here */
+  const inputArr = userInput.toString().split('');
+  const guessArr = numberToGuess.toString().split('');
+
+  let bulls = 0;
+  let cows = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (inputArr[i] === guessArr[i]) {
+      bulls++;
+    } else if (guessArr.includes(inputArr[i])) {
+      cows++;
+    }
+  }
+
+  return { bulls, cows };
 }
 
 module.exports = {
