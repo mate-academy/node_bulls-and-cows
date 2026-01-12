8dc6b4412db681884af3f26b6b614dcd2432b42a
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-01-12 21:39:41 +0200

    added solution to the task

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 'use strict';
+import readline from 'readline';
 
-// Write your code here
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+
+const randomNumber = generateRandomNumber().toString();
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askUser() {
+  terminal.question('Enter your 4-digit guess: ', (input) => {
+    if (!checkIsValidUserInput(input)) {
+      console.log(
+        'Invalid input! Make sure it is 4 unique digits and does not start with 0.',
+      );
+
+      return askUser();
+    }
+
+    const result = getBullsAndCows(input, randomNumber);
+
+    console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+
+    if (result.bulls === 4) {
+      console.log('You guessed the number!');
+      terminal.close();
+    } else {
+      askUser();
+    }
+  });
+}
+
+console.log('I have generated a 4-digit number. Try to guess it!');
+askUser();
--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,21 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const digits = [];
+
+  const firstDigit = Math.floor(Math.random() * 9) + 1;
+
+  digits.push(firstDigit);
+
+  while (digits.length < 4) {
+    const r = Math.floor(Math.random() * 10);
+
+    if (!digits.includes(r)) {
+      digits.push(r);
+    }
+  }
+
+  return Number(digits.join(''));
 }
 
 module.exports = {
