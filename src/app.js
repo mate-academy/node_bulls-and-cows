fbe81ed0bcd37d6b05a35f008612e567df7f2647
Vasyl Pryimak <priymak17ukrvaha@gmail.com>
2026-01-21 00:13:04 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
-'use strict';
+const readline = require('readline');
+const generateNumber = require('./generateNumber');
+const validateInput = require('./validateInput');
+const countBullsAndCows = require('./countBullsAndCows');
 
-// Write your code here
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const secretNumber = generateNumber();
+
+function ask() {
+  terminal.question('Enter 4 different digits: ', (input) => {
+    if (!validateInput(input)) {
+      ask();
+
+      return;
+    }
+
+    const { bulls } = countBullsAndCows(secretNumber, input);
+
+    if (bulls === 4) {
+      terminal.close();
+    } else {
+      ask();
+    }
+  });
+}
+
+ask();
