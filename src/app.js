2e221296791d6723c2742ce3a0a774f87a17be3c
Anastasia <ahinata1717@gmail.com>
2026-02-17 14:15:06 +0200

    feat: implement game bulls and cows

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// Write your code here
+import readline from 'node:readline';
+import { generateRandomNumber } from './modules/generateRandomNumber.js';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
+import { getBullsAndCows } from './modules/getBullsAndCows.js';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  'I have generated a random 4-digit number.\nTry to guess it!',
+  (userInput) => {
+    const numberToGuess = generateRandomNumber();
+
+    if (checkIsValidUserInput(userInput) === false) {
+      throw new Error('Invalid input.');
+    } else {
+      const { bulls, cows } = getBullsAndCows(
+        userInput,
+        numberToGuess.toString(),
+      );
+
+      return `Bulls: ${bulls}, Cows: ${cows}`;
+    }
+  },
+);
