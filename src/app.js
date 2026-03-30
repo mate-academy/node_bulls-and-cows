1ff7370d1217c8ec1022ca0cad85c06925850d77
Yan Kazymyr <kazymyr.yan@gmail.com>
2026-03-31 00:34:57 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
 'use strict';
+/* eslint-disable no-console */
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const targetNumber = generateRandomNumber();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function game() {
+  rl.question('Please input a 4-digits number: ', (userInput) => {
+    const validation = checkIsValidUserInput(userInput);
+
+    if (!validation) {
+      console.log(`Error: incorrect number`);
+
+      return game();
+    }
+
+    const result = getBullsAndCows(userInput, targetNumber);
+
+    console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+
+    if (result.bulls === 4) {
+      console.log(`You win! You gessed the number: ${targetNumber}`);
+      rl.close();
+    } else {
+      game();
+    }
+  });
+}
+
+console.log('Let`s play Bulls and Cows!');
+
+game();
--- src/modules/getBullsAndCows.js
+++ src/modules/getBullsAndCows.js
@@ -13,7 +13,25 @@
  * Example: { bulls: 1, cows: 2 }
  */
 function getBullsAndCows(userInput, numberToGuess) {
-  /* Write your code here */
+  const userDigits = String(userInput).split('');
+  const targetDigits = String(numberToGuess).split('');
+
+  let bulls = 0;
+  let cows = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (userDigits[i] === targetDigits[i]) {
+      bulls++;
+    }
+
+    if (targetDigits.includes(userDigits[i])) {
+      cows++;
+    }
+  }
+
+  cows -= bulls;
+
+  return { bulls, cows };
 }
 
 module.exports = {
