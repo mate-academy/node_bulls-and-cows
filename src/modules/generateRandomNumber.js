be24dde46517e1a9f184f4ba67f1acbfd5f51ded
diana <dianka_9@ukr.net>
2026-04-09 12:21:32 +0300

    Solution

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -7,7 +7,21 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  const digits = new Set();
+
+  while (digits.size < 4) {
+    const digit = Math.floor(Math.random() * 10).toString();
+
+    if (digits.size === 0 && digit === '0') {
+      continue; // Skip if the first digit is 0
+    }
+
+    if (!digits.has(digit)) {
+      digits.add(digit);
+    }
+  }
+
+  return parseInt([...digits].join(''), 10);
 }
 
 module.exports = {
