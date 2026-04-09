911a4cd4145b3133c4b60329fccf67360b759121
diana <dianka_9@ukr.net>
2026-04-09 12:35:01 +0300

    fix app

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/**
- * Generate a random 4-digit number that does not start with 0
- * and does not contain any duplicate digits.
- *
- * @return {number} A random 4-digit number
- */
 function generateRandomNumber() {
   const digits = new Set();
 
@@ -13,7 +7,7 @@ function generateRandomNumber() {
     const digit = Math.floor(Math.random() * 10).toString();
 
     if (digits.size === 0 && digit === '0') {
-      continue; // Skip if the first digit is 0
+      continue;
     }
 
     if (!digits.has(digit)) {
