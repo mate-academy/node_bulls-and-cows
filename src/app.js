14b06ef95516e6f399654eb063fa79f61de109cf
1umamaster <lukasholeg02@gmail.com>
2026-04-11 16:15:07 +0300

    The Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // Write your code here
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const numberToGuess = generateRandomNumber();
+
+module.exports = {
+  numberToGuess,
+  checkIsValidUserInput,
+  getBullsAndCows,
+};
