dd4217aaaa97af05fe72baa51aa8d6858097533f
PaulKotsan <pavlokotsan421@gmail.com>
2026-05-02 12:40:11 -0400

    Solution for Bulls&Cows

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
 'use strict';
+import readline from 'readline';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
+import { getBullsAndCows } from './modules/getBullsAndCows.js';
+import { generateRandomNumber } from './modules/generateRandomNumber.js';
 
-// Write your code here
+// Create interface for input/output
+const terminal = readline.createInterface(process.stdin, process.stdout);
+const randomNumber = generateRandomNumber();
+
+function gameLoop() {
+  // Ask player for a number
+  terminal.question('Enter four digit number: ', (playerGuess) => {
+    let result = {};
+
+    if (checkIsValidUserInput(playerGuess)) {
+      result = getBullsAndCows(+playerGuess, +randomNumber);
+    }
+
+    if (result.bulls === 4) {
+      return result;
+    } else {
+      gameLoop(); // Continue the game
+    }
+  });
+}
+
+gameLoop();
