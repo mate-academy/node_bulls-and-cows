ce8c4332b3b9a88f990094f21fe7a79321e94f76
Dima Sidenko <sidenkodima2202@gmail.com>
2026-05-26 22:54:23 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const numberToGuess = generateRandomNumber();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askGuess() {
+  rl.question('Enter your guess ', (answer) => {
+    const guess = answer.trim();
+
+    if (!checkIsValidUserInput(guess)) {
+      console.log('Invalid input! Must to 4 different digits.');
+
+      return askGuess();
+    }
+
+    const result = getBullsAndCows(guess, numberToGuess);
+
+    console.log(`Bulls: ${result.bulls}, Cows: ${result.cows}`);
+
+    if (result.bulls === 4) {
+      console.log('You guessed the number!');
+      rl.close();
+    } else {
+      askGuess();
+    }
+  });
+}
+
+askGuess();
