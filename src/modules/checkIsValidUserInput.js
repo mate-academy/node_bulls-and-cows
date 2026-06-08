439999ae4de1c82473a33349322cca6d7c3809a4
Elber-Kevenny <vieirabeb@gmail.com>
2026-06-08 20:27:25 -0300

    add task solution

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,31 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  const inputArray = userInput.split(''); // 1, 2, 3, 4
+
+  if (userInput.length !== 4) {
+    return false;
+  }
+
+  if (inputArray[0] === '0') {
+    return false;
+  }
+
+  if (userInput) {
+    for (let i = 0; i < inputArray.length; i++) {
+      if (isNaN(inputArray[i]) === true) {
+        return false;
+      }
+
+      for (let y = i + 1; y < inputArray.length; y++) {
+        if (inputArray[i] === inputArray[y]) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
 }
 
 module.exports = {
