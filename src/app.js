8f8be490595249771b211aa025a85cc74556dfef
Rostyslav <rooostik2016@gmail.com>
2026-07-27 17:16:27 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const readline = require('node:readline');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const rl = readline.createInterface({
+  input: process.input,
+  output: process.output,
+});
+
+rl.question('Input 4-digit number', (userInput) => {
+  if (!checkIsValidUserInput(userInput)) {
+    throw new Error('Invalid input');
+  }
+
+  console.log(getBullsAndCows(userInput, generateRandomNumber().toString()));
+  rl.close();
+});
