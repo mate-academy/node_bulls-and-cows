a41c12c0ec3fa37430022d37f1fa900603427f2b
PavloMolytovnyk <pavlomolytovnyk@icloud.com>
2026-08-15 18:38:37 +0300

    Fixed bugs

--- src/app.js
+++ src/app.js
@@ -1,41 +1,40 @@
 'use strict';
-
 import readline from 'node:readline';
-import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
-import { getBullsAndCows } from './modules/getBullsAndCows.js';
-import { generateRandomNumber } from './modules/generateRandomNumber.js';
+
+const checkIsValidUserInput = require('./modules/checkIsValidUserInput.js');
+const getBullsAndCows = require('./modules/getBullsAndCows.js');
+const generateRandomNumber = require('./modules/generateRandomNumber.js');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const generatedNum = generateRandomNumber();
+const genereteNum = generateRandomNumber();
 
 function askQuestion() {
-  rl.question('Type a 4-digit number from 1 to 9: ', (number) => {
-    try {
-      checkIsValidUserInput(number);
-
-      const num = +number;
-
-      const { bulls, cows } = getBullsAndCows(num, generatedNum);
-
-      if (bulls === 4) {
-        // eslint-disable-next-line no-console
-        console.log('Congratulations, you guessed all numbers!');
-        rl.close();
-
-        return;
-      }
+  rl.question('Type a 4 length number from 1 to 9: ', (number) => {
+    if (!checkIsValidUserInput(number)) {
       // eslint-disable-next-line no-console
-      console.log(`Bulls: ${bulls}, Cows: ${cows}`);
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error.message);
+      console.log('Wrong input Number');
+
+      askQuestion();
+
+      return;
     }
 
-    askQuestion();
+    const { bulls, cows } = getBullsAndCows(+number, genereteNum);
+
+    if (bulls === 4) {
+      // eslint-disable-next-line no-console
+      console.log('Congratulation, you suggest all numbers');
+      rl.close();
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`bulls: ${bulls}, cows: ${cows}`);
+
+      askQuestion();
+    }
   });
 }
 
